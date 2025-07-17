05486739fdcfe1ec36923849199c178afcb0d060
Radovan Jakubcik <rjradko104@gmail.com>
2025-07-17 23:06:11 +0200

    Testing different control methods
    + Oscillations dampener
    + Gain Scheduled PID

---
+++ MATLAB/PID_example_v01/PlotPIDResponse.m
@@ -0,0 +1,63 @@
+close all;
+clear; clc;
+
+load("data/cr_GSPID_B_8.mat");
+
+
+t = logsout.t;
+y = logsout.y;
+u = logsout.u;
+r = logsout.r;
+e = r - y;
+dt = logsout.dt;
+
+
+figure(111);
+plot(t, y, '-k', 'LineWidth', 1);
+hold on;
+plot(t, r, '-r', 'LineWidth', 1);
+title('Control Response');
+subtitle("P = " + num2str(P) + ", I = " + num2str(I) + ", D = " + num2str(D));
+legend('y(t)', 'ref(t)', "Location", "best");
+xlabel('t [s]');
+ylabel('y [deg]');
+grid on;
+hold off;
+
+figure(222);
+plot(t, u, '-k', 'LineWidth', 1);
+title('Control Output');
+subtitle("P = " + num2str(P) + ", I = " + num2str(I) + ", D = " + num2str(D));
+legend('u(t)', "Location", "best");
+xlabel('t [s]');
+ylabel('u [%]');
+grid on;
+
+figure(666);
+plot(t, y, '-k', 'LineWidth', 1);
+hold on;
+plot(t, r, '-r', 'LineWidth', 1);
+plot(t, e, '-b', 'LineWidth', 1);
+title('Control Error Response');
+subtitle("P = " + num2str(P) + ", I = " + num2str(I) + ", D = " + num2str(D));
+legend('y(t)', 'ref(t)', 'e(t)', "Location", "best");
+xlabel('t [s]');
+ylabel('Value [deg]');
+grid minor;
+hold off;
+
+
+if exist("RegParams", "var")
+    figure(777);
+    plot(t, RegParams(:, 1), '-k', 'LineWidth', 1);
+    hold on;
+    plot(t, RegParams(:, 2), '-r', 'LineWidth', 1);
+    plot(t, RegParams(:, 3), '-b', 'LineWidth', 1);
+    title('Gain Scheduling');
+    subtitle("PID Params");
+    legend('P', 'I', 'D', "Location", "best");
+    xlabel('t [s]');
+    ylabel('Value [-]');
+    grid minor;
+    hold off;
+end
